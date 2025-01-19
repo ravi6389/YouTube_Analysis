c71456c52908160f20ce5f98a3fb665bd6e6c1ae
ravi6389 <77967899+ravi6389@users.noreply.github.com>
2025-01-20 01:55:59 +0530

    Update video_text.py

--- video_text.py
+++ video_text.py
@@ -65,6 +65,7 @@ if not video_url:
 
 # Fetch Transcript
 transcript = get_transcript(video_url)
+st.write(transcript)
 if isinstance(transcript, str):  # Error or no transcript found
     st.error(transcript)
 else:
